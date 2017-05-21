c5f4fb7b58ec64855d93936689531923ec127f6a
Armen Gabrielyan <armengabrielyan16@gmail.com>
2017-05-21 16:44:17 +0400

    Add String Algorithms
     *Wrote [charAt()] algorithm for C, C++,
     C# and Java languages
     *Improved [charAt()] algorithm for
     JavaScript language

--- JavaScript/Algorithms/String/charAt.js
+++ JavaScript/Algorithms/String/charAt.js
@@ -6,9 +6,17 @@
  * @returns {string}
  */
 function charAt(string, index) {
+    if (typeof string != 'string') {
+        throw new TypeError('Parameter is not of string type');
+    } else if (index !== parseInt(index)) {
+        throw new TypeError('Parameter is not of int type');
+    } else if (index < 0 || index > string.length - 1) {
+        throw new RangeError('Index is out of range');
+    }
+
     try {
-        return string.charAt(index);
+        return string[index];
     } catch (err) {
-        console.log(err.message);
+        console.error(err.message);
     }
 }
