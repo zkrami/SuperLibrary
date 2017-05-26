d4dfaba06f9c86293f2de038cd4aedb87ab787e0
ArmenGabrielyan <armengabrielyan16@gmail.com>
2017-05-27 02:01:08 +0400

    Add Math Algorithm: Fibonacci Algorithm
     *Added both recursive and iterative
      versions of Fibonacci algorithm for
     C, C++, C#

---
+++ C#/Algorithms/Math/Fibonacci.cs
@@ -0,0 +1,25 @@
+/**
+ * Class Fibonacci
+ */
+public class Fibonacci {
+
+    /**
+     * Calculates the Fibonacci number at the given index using iteration
+     *
+     * @param int index
+     * @return int
+     */
+    public int fibonacciIteration(int index) {
+        int a = 0;
+        int b = 0;
+        int c = 1;
+
+        for (int i = 0;i < index;i++) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+
+        return c;
+    }
+}
