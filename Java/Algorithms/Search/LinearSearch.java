536514bafb373388bbfcd4798565e82b46956c25
ArmenGabrielyan <armengabrielyan16@gmail.com>
2017-05-30 23:19:40 +0400

    Add Search Algorithm: Linear Search Algorithm
     *Added Linear Search algorithm for
     Java, JavaScript, and PHP

---
+++ Java/Algorithms/Search/LinearSearch.java
@@ -0,0 +1,24 @@
+/**
+ * Class LinearSearch
+ */
+public class LinearSearch {
+
+    /**
+     * Implements Linear Search algorithm
+     *
+     * @param array a given array in which the wanted element is searched
+     * @param searchingElement the element to be searched for
+     * @returns {*}
+     */
+    public int linearSearch(int[] array, int searchingElement) {
+        int arrayLength = array.length;
+
+        for (int i = 0;i < arrayLength;i++) {
+            if (array[i] == searchingElement) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ PHP/Algorithms/Search/linearSearch.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements Linear Search algorithm
+ *
+ * @param array $array
+ * @param mixed $searchingElement
+ * @return int|string
+ */
+function linearSearch(array $array, $searchingElement)
+{
+    foreach ($array as $key => $value) {
+        if ($value == $searchingElement) {
+            return $key;
+        }
+    }
+
+    return 'Not found';
+}
