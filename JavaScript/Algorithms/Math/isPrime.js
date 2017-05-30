d1cfcbe9351564b6d161ecfc1cb3682b31da9acd
ArmenGabrielyan <armengabrielyan16@gmail.com>
2017-05-30 15:57:27 +0400

    Add Math Algorithm: Prime Number Algorithm
     *Added Prime Number algorithm for
     Java, JavaScript, and PHP

---
+++ JavaScript/Algorithms/Math/isPrime.js
@@ -0,0 +1,16 @@
+/**
+ * Checks if a given number is prime
+ *
+ * @param {int} number
+ * @return bool
+ */
+function isPrime(number)
+{
+    for (var i = 2;i <= number / 2;i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
