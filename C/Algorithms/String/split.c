158dfb2ce6a445a2e267368f75f439f7c285b26f
Armen Gabrielyan <armengabrielyan16@gmail.com>
2017-06-07 14:58:31 +0400

    Update split.c
    *Reformatted code

--- C/Algorithms/String/split.c
+++ C/Algorithms/String/split.c
@@ -13,20 +13,19 @@ void split(char *string, char delimiter, char *words[])
 {
     int i = -1, j = 0, k = 0;
     do {
-	    i++;
-	    if (string[i] == delimiter || string[i] == '\0') {
-	        char *subString = malloc(sizeof(char) * 20); //subString = max length of individual words
-	        for (int a = j, b = 0; a <= i; a++, b++) {    
-		        if (a == i) {
-		        subString[b] = '\0';
-		        }
-		        else {
-		            subString[b] = string[a];
-		        }
+        i++;
+        if (string[i] == delimiter || string[i] == '\0') {
+            char *subString = malloc(sizeof(char) * 20); //subString = max length of individual words
+            for (int a = j, b = 0; a <= i; a++, b++) {
+                if (a == i) {
+                    subString[b] = '\0';
+                } else {
+                    subString[b] = string[a];
+                }
             }
             words[k] = subString;
             k++;
-            j = i + 1;			
+            j = i + 1;
         }
-	} while (string[i] != '\0');
+    } while (string[i] != '\0');
 }
